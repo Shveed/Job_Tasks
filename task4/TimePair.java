1c3b6d01bdeee47edc40fed59a29c14ceecf397c
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2019-07-31 18:29:38 +0300

    Create TimePair.java

---
+++ task4/TimePair.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TimePair {
+     int startMinutes;
+     int endMinutes;
+
+     public TimePair(int start, int end){
+         this.startMinutes = start;
+         this.endMinutes = end;
+     }
+}
