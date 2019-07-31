db25cb41cbd986fb5546d30eac643312ef14a850
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2019-07-31 15:07:37 +0300

    Update Main.java

--- task3/Main.java
+++ task3/Main.java
@@ -1,31 +1,45 @@
 package com.company;
-
+//////////////////////////////////////////////////////////////////
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
+//////////////////////////////////////////////////////////////////
 public class Main {
     final static int CASH_AMOUNT = 5;
-    final static int INTERVALS = 16;
+    final static int INTERVALS   = 16;
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        double[][] cashAndInterval = new double[CASH_AMOUNT][INTERVALS];
+        double[][] cashAndInterval =
+                new double[CASH_AMOUNT][INTERVALS];
         System.out.println("Enter path");
         String filesPath = sc.next();
+////////File reading process//////////////////////////////////////
         for(int i = 0; i < CASH_AMOUNT; i++){
             int interval = 0;
-            FileInputStream fis = new FileInputStream(filesPath + "\\Cash" + (i+1) + ".txt");
-            BufferedReader bf = new BufferedReader(new InputStreamReader(fis));
+            FileInputStream fis =
+                    new FileInputStream(
+                            filesPath +
+                                    "\\Cash" + (i+1) + ".txt"
+                                        );
+            BufferedReader bf =
+                    new BufferedReader(
+                            new InputStreamReader(fis)
+                                        );
+////////////Reading file data/////////////////////////////////////
             String inputLine;
             while((inputLine = bf.readLine()) != null){
-                cashAndInterval[i][interval] = Double.parseDouble(inputLine.substring(0, inputLine.indexOf("\\")));
+                cashAndInterval[i][interval] =
+                        Double.parseDouble(
+                                inputLine.substring(0,
+                                        inputLine.indexOf("\\")));
                 interval++;
             }
             fis.close();
             bf.close();
         }
+////////Counting maximum cash loading/////////////////////////////
         double max = 0;
         int maxIndex = 0;
         for(int j = 0; j < 16; j++){
